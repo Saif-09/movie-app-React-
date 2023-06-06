263a8ab235964ac7622464f614dc5ca4c28ba579
Saif-09 <saifmd238@gmail.com>
2023-06-06 14:27:47 +0530

    using state

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,26 +1,68 @@
 import React from "react";
 
 class MovieCard extends React.Component{
-    render(){
-        return(
-            <div className="main">
-                <div className="movie-card"></div>
-                <div className="left">
-                    <img alt="Poster"/>
-                </div>
-                <div className="right">
-                    <div className="title">The adventures</div>
-                    <div className="plot">Plot of the movie will be here</div>
-                    <div className="price">Rs.299</div>
 
-                    <div className="footer">
-                        <div className="star-rating">8.9</div>
-                        <div className="stars">star</div>
-                        <button className="favourite">Favorite</button>
-                        <button className="cart-btn">Add to cart</button>
+    constructor(){
+        super();
+        this.state = {
+            title: "The Avengers",
+            plot:"Supernatural powers shown in the movie",
+            price:199,
+            rating:8.9
+        }
+    }
+
+    render(){
+          const {title,plot,price,rating} = this.state;
+        return(
+
+            <div className="main">
+
+                {/**Movie Card */}
+                <div className="movie-card">
+
+                    {/**Left section of Movie Card */}
+                    <div className="left">
+                        <img alt="poster" src='https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg' />
                     </div>
+                    
+                    {/**Right section Movie Card */}
+                    <div className="right">
+
+                        {/**Title, plot, price of the movie */}
+                        <div className="title">{title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">Rs. {price}</div>
+
+                        {/**Footer starts here with ratings, stars and buttons */}
+                        <div className="footer">
+                            <div className="rating">{rating}</div>
+
+                            {/**Star image with increase and decrease buttons and star count */}
+                            <div className="star-dis">
+                                <img className="str-btn" 
+                                    alt="Decrease" 
+                                    src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" 
+                                />
+                                <img className="stars" 
+                                        alt="stars" 
+                                        src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"    
+                                />
+                                <img className="str-btn" 
+                                    alt="increase" 
+                                    src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png" 
+                                />
+                                <span className="starCount">0</span>
+                            </div>
+
+                            {/**Favourite and add to cart buttons */}
+                            <button className="favourite-btn">Favourite</button>
+                            <button className="cart-btn">Add to Cart</button>
+                            
+                        </div>
+                    </div>
+
                 </div>
-                
             </div>
         )
     }
