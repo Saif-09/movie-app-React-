d8c1e809eaf7f70b6e96b5b025fc5d4d5d18b982
Saif-09 <saifmd238@gmail.com>
2023-06-08 04:10:08 +0530

    using styled-components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+
+
+//This is react library -styled components is used
+const Nav = styled.div`
+ width: 100%;
+  height: 70px;
+  background-color: #4267b2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`;
+const Title = styled.div`
+font-size: 30px;
+color: #fff;
+font-weight:600;
+font-family: Monsterrat,sans-serif;
+text-transform: uppercase;
+margin-left:20px;
+`;
+
+const CartContainer = styled.div`
+        position: relative;
+        cursor:pointer;
+
+`;
+
+const CartIcon = styled.img`
+        height:48px;
+        margin-right:20px;
+`;
+
+const CartCount = styled.span`
+        background:orange;
+        border-radius:50%;
+        padding: 4px 8px;
+        position: absolute;
+        right:10px;
+        top:-5px;
+        font-size:12px;
+
+`
 
 class Navbar extends React.Component{
     render(){
         return(
             <>
-            <div style={styles.nav}>
-                <div style={styles.title}>Movie-App</div>
-                <div style={styles.cartContainer}>
-                    <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt="Cart Item"/>
-                    <span style={styles.cartCount}>0</span>
-                </div>
-            </div>
+            <Nav>
+                <Title>Movie-App</Title>
+                <CartContainer>
+                    <CartIcon src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt="Cart Item"/>
+                    <CartCount>0</CartCount>
+                </CartContainer>
+            </Nav>
             </>
         )
     }
@@ -24,41 +67,44 @@ export default Navbar;
 //   display: flex;
 //   justify-content: space-between;
 
-const styles = {
-    nav: {
-        height:70,
-        background:"#4267b2",
-        display:"flex",
-        justifyContent:"space-between",
-        alignItems:"center",
-        position:"relative"
-    },
-    title: {
-        fontSize: 30,
-        color: "#fff",
-        fontWeight:600,
-        fontfamily: '"Monsterrat",sans-serif',
-        textTransform: "uppercase",
-        marginLeft:20
 
-    },
-    cartContainer: {
-        position: "relative",
-        cursor:"pointer",
+//This is one of the way to use CSS (internal css)
 
-    },
-    cartIcon:{
-        height:48,
-        marginRight:20
-    },
-    cartCount:{
-        background:"orange",
-        borderRadius:"50%",
-        padding: "4px 8px",
-        position: "absolute",
-        right:10,
-        top:-5,
-        fontSize:12
-    }
+// const styles = {
+//     nav: {
+//         height:70,
+//         background:"#4267b2",
+//         display:"flex",
+//         justifyContent:"space-between",
+//         alignItems:"center",
+//         position:"relative"
+//     },
+//     title: {
+//         fontSize: 30,
+//         color: "#fff",
+//         fontWeight:600,
+//         fontfamily: '"Monsterrat",sans-serif',
+//         textTransform: "uppercase",
+//         marginLeft:20
 
-}
+//     },
+//     cartContainer: {
+//         position: "relative",
+//         cursor:"pointer",
+
+//     },
+//     cartIcon:{
+//         height:48,
+//         marginRight:20
+//     },
+//     cartCount:{
+//         background:"orange",
+//         borderRadius:"50%",
+//         padding: "4px 8px",
+//         position: "absolute",
+//         right:10,
+//         top:-5,
+//         fontSize:12
+//     }
+
+// }
