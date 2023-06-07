ee6a6283f9a5d6250f8de7ba8ee7964954d64ee1
Saif-09 <saifmd238@gmail.com>
2023-06-08 02:37:00 +0530

    using inline css for styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends React.Component{
     render(){
         return(
             <>
-            <div className="nav">
+            <div style={{width:"100%" , height:70 , background:"aqua" , display: "flex", justifyContent: "space-between"}}>
                 <div className="title">Movie-App</div>
                 <div className="cart">
                     <img alt="Cart Item"/>
@@ -17,3 +17,9 @@ class Navbar extends React.Component{
 }
 
 export default Navbar;
+
+// width: 100%;
+//   height: 70px;
+//   background-color: aqua;
+//   display: flex;
+//   justify-content: space-between;
