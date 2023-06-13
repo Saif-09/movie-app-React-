21691bd3f037405ba421363aa2e02ea9c8a08f73
Saif-09 <saifmd238@gmail.com>
2023-06-14 02:33:50 +0530

    arranged all the movie data in a file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
+import MovieList from "./MovieList";
+import Navbar from "./Navbar";
+import Navbar1 from "./Navbar1";
+import { movies } from "./movies.data";
 
-import MovieList from './MovieList';
-import Navbar from './Navbar';
-import Navbar1 from './Navbar1';
+class App extends React.Component {
 
-function App() {
-  return (
-    <>
-    
-    <Navbar/>
-    <MovieList/>
-    {/* <Navbar1/> */}
-    </>
-  );
+  constructor() {
+    super();
+    this.state = {
+      movies: movies,
+      cartCount:0
+  }
+  }
+  handleIncStar = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+
+    if (movies[mid].star >= 10) {
+      return;
+    }
+
+    movies[mid].star += 0.5;
+    this.setState({
+      movies: movies,
+    });
+  };
+
+  handleDecStar = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+    if (movies[mid].star <= 0) {
+      return;
+    }
+    movies[mid].star -= 0.5;
+    this.setState({
+      movies: movies,
+    });
+  };
+
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+    movies[mid].fav = !movies[mid].fav;
+    this.setState({
+      movies: movies,
+    });
+  };
+
+  handleAddtocart = (mov) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(mov);
+    movies[mid].isInCart = !movies[mid].isInCart;
+    this.setState({
+      movies: movies,
+    });
+  };
+
+  render() {
+    const { movies, cartCount } = this.state;
+    return (
+      <>
+        <Navbar />
+        <MovieList movies={movies}
+                   addStars ={this.handleIncStar}
+                   decStars = {this.handleDecStar}
+                   toggleFav = {this.handleToggleFav}
+                   toggleCart = {this.handleAddtocart} 
+                   cartCount = {cartCount}
+                   />
+        {/* <Navbar1/> */}
+      </>
+    );
+  }
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-
 //This is react library -styled components is used
 const Nav = styled.div`
- width: 100%;
+  width: 100%;
   height: 70px;
   background-color: #4267b2;
   display: flex;
@@ -13,52 +12,57 @@ const Nav = styled.div`
   position: relative;
 `;
 const Title = styled.div`
-font-size: 30px;
-color: #fff;
-font-weight:600;
-font-family: Monsterrat,sans-serif;
-text-transform: uppercase;
-margin-left:20px;
-&:hover{color:red}//like this we can use pseudo classes
+  font-size: 30px;
+  color: #fff;
+  font-weight: 600;
+  font-family: Monsterrat, sans-serif;
+  text-transform: uppercase;
+  margin-left: 20px;
+  &:hover {
+    color: red;
+  } //like this we can use pseudo classes
 `;
 
 const CartContainer = styled.div`
-        position: relative;
-        cursor:pointer;
-
+  position: relative;
+  cursor: pointer;
 `;
 
 const CartIcon = styled.img`
-        height:48px;
-        margin-right:20px;
+  height: 48px;
+  margin-right: 20px;
 `;
 
 const CartCount = styled.span`
-        background:${(props)=>props.color};
-        border-radius:50%;
-        padding: 4px 8px;
-        position: absolute;
-        right:10px;
-        top:-5px;
-        font-size:12px;
-        display: ${(props)=>props.show?"":"none"};
-
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  padding: 4px 8px;
+  position: absolute;
+  right: 10px;
+  top: -5px;
+  font-size: 12px;
+  display: ${(props) => (props.show ? "" : "none")};
 `;
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <>
-            <Nav>
-                <Title>Movie-App</Title>
-                <CartContainer>
-                    <CartIcon src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt="Cart Item"/>
-                    <CartCount color="red" show={false}>3</CartCount>
-                </CartContainer>
-            </Nav>
-            </>
-        )
-    }
+class Navbar extends React.Component {
+  render() {
+    return (
+      <>
+        <Nav>
+          <Title>Movie-App</Title>
+          <CartContainer>
+            <CartIcon
+              src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+              alt="Cart Item"
+            />
+            <CartCount color="red" show={true}>
+              3
+            </CartCount>
+          </CartContainer>
+        </Nav>
+      </>
+    );
+  }
 }
 
 export default Navbar;
@@ -68,7 +72,6 @@ export default Navbar;
 //   background-color: aqua;
 //   display: flex;
 //   justify-content: space-between;
-
 
 //This is one of the way to use CSS (internal css)
 
